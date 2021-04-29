277188dbdfb45cb0a19a59408a023c4db43401ee
segur <seguro17@gmail.com>
2021-04-29 20:58:38 +0100

    Validação dos layouts

--- app/src/main/java/vitor/treino/informacao/MainActivity.kt
+++ app/src/main/java/vitor/treino/informacao/MainActivity.kt
@@ -2,7 +2,6 @@ package vitor.treino.informacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -13,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    //todo: Função do botão, com o id: "Button".
     fun sendInformation(view: View) {
 
         //todo: Encontrar os layouts de informação e incrementa los numa variável.
@@ -20,5 +20,26 @@ class MainActivity : AppCompatActivity() {
         val EditTextAge = findViewById<EditText>(R.id.EditTextAge)
         val EditTextEmail = findViewById<EditText>(R.id.EditTextEmail)
         val EditTextPhone = findViewById<EditText>(R.id.EditTextPhone)
+
+        //todo: Guardar o conteúdo do layout em forma de texto.
+        val mesageName = EditTextName.text.toString()
+        val mesageAge = EditTextAge.text.toString()
+        val mesageEmail = EditTextEmail.text.toString()
+        val mesagePhone = EditTextPhone.text.toString()
+
+        //todo: Validação do texto nas layouts.
+        if (mesageName.isBlank() ) {
+            EditTextName.error = "Invalid Name."
+        }
+        if(mesageAge.isBlank()){
+            EditTextAge.error = "Invalid Age."
+        }
+        if(mesageEmail.isBlank()){
+            EditTextEmail.error = "Invalid Email."
+        }
+        if(mesagePhone.isBlank()){
+            EditTextPhone.error = "Invalid Phone."
+        }
+
     }
 }
