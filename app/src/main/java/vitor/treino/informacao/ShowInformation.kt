6e2f6025a23eed97439f1c600d25ad3987dee9d1
segur <seguro17@gmail.com>
2021-04-29 22:03:41 +0100

    Apresentação dos dados

--- app/src/main/java/vitor/treino/informacao/ShowInformation.kt
+++ app/src/main/java/vitor/treino/informacao/ShowInformation.kt
@@ -8,18 +8,23 @@ class ShowInformation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_information)
+
+        //todo: Guardar o conteúdo do "pacote" enviado numa variavel
+        val mesageName = intent.getStringExtra(MainActivity.INFO_EXTRA_NAME)
+        val mesageAge = intent.getStringExtra(MainActivity.INFO_EXTRA_AGE)
+        val mesageEmail = intent.getStringExtra(MainActivity.INFO_EXTRA_EMAIL)
+        val mesagePhone = intent.getStringExtra(MainActivity.INFO_EXTRA_PHONE)
+
+        //todo: Encontrar os layouts de informação e incrementa los numa variável.
+        val textViewName = findViewById<TextView>(R.id.textViewName)
+        val textViewAge = findViewById<TextView>(R.id.textViewAge)
+        val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
+        val textViewPhone = findViewById<TextView>(R.id.textViewPhone)
+
+        //todo: Apresentar as Informações enviadas nos layouts difenidos
+        textViewName.setText(mesageName)
+        textViewAge.setText(mesageAge)
+        textViewEmail.setText(mesageEmail)
+        textViewPhone.setText(mesagePhone)
     }
-
-    //todo: Encontrar os layouts de informação e incrementa los numa variável.
-    val textViewName = findViewById<TextView>(R.id.textViewName)
-    val textViewAge = findViewById<TextView>(R.id.textViewAge)
-    val textViewEmail = findViewById<TextView>(R.id.textViewEmail)
-    val textViewPhone = findViewById<TextView>(R.id.textViewPhone)
-
-    //todo: Guardar o conteúdo do "pacote" enviado numa variavel
-    val mesageName = intent.getStringArrayExtra(MainActivity.INFO_EXTRA_NAME)
-    val mesageAge = intent.getStringArrayExtra(MainActivity.INFO_EXTRA_AGE)
-    val mesageEmail = intent.getStringArrayExtra(MainActivity.INFO_EXTRA_EMAIL)
-    val mesagePhone = intent.getStringArrayExtra(MainActivity.INFO_EXTRA_PHONE)
-
 }
